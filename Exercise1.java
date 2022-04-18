bb5cdddc0c65d09e4f163b28ba1bbc6782eac2c4
suvarna272 <64902474+suvarna272@users.noreply.github.com>
2022-04-18 12:32:28 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,11 @@
+// Write a program to print your Name, Father's Name and Mailing Address in different lines
+
+class Exercise1
+{
+  public static void main(String args[])
+  {
+    System.out.println("John Raju");
+    System.out.println("suvarna babu");
+    System.out.println("Satyavathi");
+  }
+} 
---
+++ Sample1.java
@@ -0,0 +1,10 @@
+// Write a program to print your name
+
+class Sample1
+{
+	public static void main(String args[]) 
+  {
+    System.out.println("Suvarna Babu");
+
+	}
+}
